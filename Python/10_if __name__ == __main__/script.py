268ed7c9e10ce80f3442939842ae691c28843c00
Alok Pahan <76406541+Alokpahan@users.noreply.github.com>
2023-10-05 19:03:02 +0530

    Add files via upload

--- Python/10_if __name__ == __main__/script.py
+++ Python/10_if __name__ == __main__/script.py
@@ -1,5 +1,12 @@
-
 if __name__ == "__main__":      # This code block will only execute if this script is run directly
     print("This script is being run directly.")
 else:
-    print("This script is being imported as a module into another script.") # This code block will only execute if this script is run Indirectly(through another script)
+    # This code block will only execute if this script is run Indirectly(through another script)
+    print("This script is being imported as a module into another script.")
+    
+ 
+# When you run this script directly, you will see the output: 
+# "This script is being run directly." 
+# However, if you import this script as a module into another script, you will see the output: 
+# "This script is being imported as a module into another script." 
+# This allows you to reuse code from one script in another while controlling the execution of specific parts of the code.    
