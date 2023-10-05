b5618c8dd248dd994ba573af8104605f12e5fbbf
Alok Pahan <76406541+Alokpahan@users.noreply.github.com>
2023-10-05 19:01:48 +0530

    Add files via upload

---
+++ Python/10_if __name__ == __main__/script.py
@@ -0,0 +1,5 @@
+
+if __name__ == "__main__":      # This code block will only execute if this script is run directly
+    print("This script is being run directly.")
+else:
+    print("This script is being imported as a module into another script.") # This code block will only execute if this script is run Indirectly(through another script)
